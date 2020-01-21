32832dabc181eef938ffbd78c4bd007db629fc1e
Wensheng Chen <hi@wenshengchen.com>
2020-01-21 03:02:32 -0800

    Remove public resource from the server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -11,7 +10,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
